21f2bcb10cacd0e6674eea5c29180aaa4261a170
narek <narench@inbox.ru>
2022-12-13 00:28:29 +0300

    Added unarchive function with bugs | update cmake

--- main.cpp
+++ main.cpp
@@ -3,9 +3,16 @@
 using namespace std;
 
 void archive(string file_name);
+void unarchive(string file_name);
 
 int main(int argc, char* argv[]){
-    if (argv[1] == string("archive")) archive(string(argv[2]));
+    if (argc == 3) {
+        if (argv[1] == string("archive")) archive(string(argv[2]));
+        else if (argv[1] == string("unarchive")) unarchive(string(argv[2]));
+        else cout << "Uncorrect opperation" << endl;
+    }
+
+    else cout << "Uncorrect opperation" << endl;
 
     return 0;
 }
---
+++ unarchive.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void unarchive(string file_name){
+    ifstream input(file_name, ios_base::binary);
+    ofstream output("ouput.txt", ios_base::binary);
+
+    input.seekg(0, ios_base::end);
+    int sizeInByte = input.tellg();
+    input.seekg(0, ios_base::beg);
+    
+    unsigned char tmp1;
+    unsigned char tmp2;
+
+    for (int i  = 0; i < sizeInByte; i++){
+        input.read((char*)&tmp1, sizeof(char));
+        if (tmp1 > 0x81){
+            input.read((char*)&tmp2, sizeof(char));
+            for (int j = 0; j < (tmp1 - 0x80); j++) output.write((char*)&tmp2, sizeof(char));
+        }
+        output.write((char*)&tmp1, sizeof(char));
+    }
+
+}
